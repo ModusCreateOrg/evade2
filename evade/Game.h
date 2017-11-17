1cca2ddfedac2104494dc424701b4187ffb65991
Michael Schwartz <mykesx@gmail.com>
2017-11-17 15:49:08 -0800

    wip

--- evade/Game.h
+++ evade/Game.h
@@ -2,12 +2,14 @@
 #define GAME_H
 #include "Types.h"
 
+const int NUM_STARS = 25;
 const int NUM_OBJECTS = 4;
 const int NUM_PROCESSES = 4;
 
 const BYTE PTYPE_USER = 0;
 const BYTE PTYPE_SYSTEM = 1;
 
+class Starfield;
 class Object;
 class ObjectManager;
 class Process;
@@ -17,5 +19,7 @@ class ProcessManager;
 #include "ObjectManager.h"
 #include "Process.h"
 #include "ProcessManager.h"
+#include "Starfield.h"
+#include "Player.h"
 
 #endif
