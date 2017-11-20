1bb9cc5c366a027aabf8710aeb968232662e5b05
Michael Schwartz <mykesx@gmail.com>
2017-11-20 10:21:33 -0800

    comments

--- evade/Fighter1.h
+++ evade/Fighter1.h
@@ -1,6 +1,14 @@
 #ifndef FIGHTER1_H
 #define FIGHTER1_H
 
+/**
+ * Fighter1 process.
+ *
+ * We only expose the initial state/function as extern here, so it can be ProcessManager::birth()
+ * from anywhere.
+ *
+ * The remaining states are static/local to the Fighter1.cpp file.
+ */
 extern void fighter1_process(Process *me);
 
 #endif
--- evade/Figther1.cpp
+++ evade/Figther1.cpp
@@ -2,12 +2,27 @@
 
 #include "fighter1_img.h"
 
+/**
+ * Initialize the figther Object's position and velocity.
+ *
+ * For demonstration purposes, the Fighter only moves in Z, and faster than
+ * the camera so it will shrink as it goes into the distance.
+ *
+ * The Object is initialized right at the camera position so it will appear
+ * right away.
+ */
 static void init(Object *o) {
   o->x = Camera::x; // 64 - (COORD)random(0, 128) + Camera::x;
   o->y = Camera::y; // 32 - (COORD)random(0, 64) + Camera::y;
   o->z = Camera::z; // (COORD)random(200, 255);
   o->vz = 6;
 }
+
+/**
+ * Wait until the Object's Z is far into the distance.
+ *
+ * Then we re-initialize the Object.
+ */
 static void wait(Process *me) {
   Object *o = me->o;
 
@@ -17,10 +32,17 @@ static void wait(Process *me) {
   me->sleep(1, wait);
 }
 
+/*
+ * fighter1_process
+ *
+ * Initial state/entry point for the Fighter1 Process.
+ *
+ * Allocates an Object and sets its image (lines).
+ */
 void fighter1_process(Process *me) {
   Object *o = ObjectManager::alloc();
   me->o = o;
   o->lines = fighter1_img;
   init(o);
-  me->sleep(1, wait);
+  me->sleep(1, wait);   // next frame we resume running the wait() state.
 }
