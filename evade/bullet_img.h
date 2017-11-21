88c994b7a230eaee8c6c665846145b1fe3c14162
Jay Garcia <jesusdgarcia@gmail.com>
2017-11-21 14:50:32 -0500

    Updated SVG images to use the new format

--- evade/bullet_img.h
+++ evade/bullet_img.h
@@ -2,27 +2,22 @@
 #define BULLET_IMG_H
 
 #include "Types.h"
-// drawLine(x0, y0, x1, y1, color);
 
-// GRAPHIC: fighter1.svg
+
 // Number bytes 52
 const PROGMEM UBYTE bullet1_img[] = {
-  16, 2,
+  16, // Width
+  16, // height
+  2,  // num rows
   0, 0, 15, 15,
-  0, 15, 15, 0,
-  0, 0, 15, 0,
-  15, 0, 15, 15,
-  0, 15, 15, 15,
-  0, 15, 0, 0,
+  0, 15, 15, 0
 };
 
 const PROGMEM UBYTE bullet2_img[] = {
-  16, 2,
+  16, // Width
+  16, // height
+  2,  // num rows
   8, 0, 8, 15,
-  0, 8, 15, 8,
-  0, 0, 15, 0,
-  15, 0, 15, 15,
-  0, 15, 15, 15,
-  0, 15, 0, 0,
+  0, 8, 15, 8
 };
 #endif
