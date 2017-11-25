188f7b5b55f552712eb87732942429d7400c33c8
Jay Garcia <jesusdgarcia@gmail.com>
2017-11-25 12:20:56 -0500

    Updated convert-lines.js to output the new data format.

--- svg/convert-lines.js
+++ svg/convert-lines.js
@@ -2,13 +2,34 @@
 
 const svgson = require('svgson'),
       colors = require('colors'),
-      argv = require('minimist')(process.argv.slice(2));
+      argv = require('minimist')(process.argv.slice(2)),
+      varName = argv.v;
 
 // From .svg file
 const fs = require('fs');
 
 function stringify(d) {
     return JSON.stringify(d, null, 4);
+}
+
+function syntax() {
+    console.log(`
+Syntax: ./convert-lines.js -i <input_file> -v <output_variable_name> 
+`);
+}
+
+function error(str) {
+    console.log(('Error! ' + str).red);
+    syntax();
+    process.exit(1);
+}
+
+if (! argv.i) {
+    error('No input file indicated!');
+}
+
+if (! argv.v) {
+    error('No output variable name specified!');
 }
 
 const data = fs.readFileSync(argv.i, 'utf-8');
@@ -23,8 +44,8 @@ function hexify (number, nopadding) {
 }
 
 function processResult(svgJSON) {
-    console.log(stringify(svgJSON));
-    return;
+    // console.log(stringify(svgJSON));
+    // return;
     var varName = argv.i.split('.')[0];
 
     var output = '',
@@ -32,15 +53,14 @@ function processResult(svgJSON) {
         tab = '\t0x',
         EOL = ',\n',
         hexPrefix = '',
-        children = svgJSON.myPaths.childs[1].childs;
+        children = svgJSON.myPaths.childs[1].childs,
+        attrs  = svgJSON.myPaths.attrs,
+        dimensions = [attrs.width, attrs.height];
 
     children.forEach(function(child, index) {
         if (child.name == 'line') {
             // console.log(child);
             numLines++;
-
-
-           
 
             // output += '\n';
 
@@ -49,10 +69,6 @@ function processResult(svgJSON) {
                     (tab + hexify(child.attrs.y1)),
                     (tab + hexify(child.attrs.x2)),
                     (tab + hexify(child.attrs.y2)),
-                    // +Math.round(child.attrs.x1),
-                    // +Math.round(child.attrs.y1),
-                    // +Math.round(child.attrs.x2),
-                    // +Math.round(child.attrs.y2),
                 ].toString() + (index < children.length - 1 ? ',' : '');
 
              output += [
@@ -60,10 +76,6 @@ function processResult(svgJSON) {
                     ' y1:' + Math.round(child.attrs.y1),
                     ' x2:' + Math.round(child.attrs.x1),
                     ' y2:'+ Math.round(child.attrs.y2),
-                    // +Math.round(child.attrs.x1),
-                    // +Math.round(child.attrs.y1),
-                    // +Math.round(child.attrs.x2),
-                    // +Math.round(child.attrs.y2),
                 ].toString();
 
             output += (index < children.length - 1 ?  EOL : '');
@@ -72,16 +84,18 @@ function processResult(svgJSON) {
     });
 
 
+
 console.log(`
-// GRAPHIC: ${argv.i}
-// Number bytes ${numLines * 4}
+// SVG Graphic source: ${argv.i}
+// Number bytes ${(numLines * 4) + 3}
 const PROGMEM uint8_t ${varName}[] = {
-${tab}${hexify(numLines).toUpperCase()}, // Number of rows of coords
+${tab}${hexify(dimensions[0])},\t// Width (${dimensions[0]} px)
+${tab}${hexify(dimensions[1])},\t// Height (${dimensions[1]} px)
+${tab}${hexify(numLines)},\t// Number of rows of coords (${numLines})
 ${output}
 };`);
 
-}
-
+} //
 
 svgson(data, {
     // svgo: true,
