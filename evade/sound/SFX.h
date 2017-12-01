a38688db960df299203555c0d5c7bdebe198174f
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-01 17:42:58 -0500

    Adding new SFX header file to encapsulate sfx :)

---
+++ evade/sound/SFX.h
@@ -0,0 +1,25 @@
+#ifndef SFX_H
+#define SFX_H
+
+
+const PROGMEM struct sfx1_data {
+  uint8_t fmt;
+  uint8_t pattern0[13];
+} pew = {
+  .fmt = ATM_SCORE_FMT_MINIMAL_MONO,
+  .pattern0 = {
+    ATM_CMD_M_SET_TEMPO(22),
+    ATM_CMD_M_SET_VOLUME(63),
+    ATM_CMD_I_NOTE_F5,
+    ATM_CMD_M_DELAY_TICKS(3),
+    ATM_CMD_I_NOTE_C4,
+    ATM_CMD_M_DELAY_TICKS(2),
+    ATM_CMD_I_NOTE_C3,
+    ATM_CMD_M_DELAY_TICKS(2),    
+    ATM_CMD_I_NOTE_OFF,
+    ATM_CMD_M_DELAY_TICKS(11),
+    ATM_CMD_I_STOP,
+  },
+};
+
+#endif
