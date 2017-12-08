d93af923f2f5fada90fd7ca8a4bddd5c21819c9b
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-08 09:35:42 -0500

    Saved some 12 bytes of Flash

--- evade/img/enemy_scout_2_img.h
+++ evade/img/enemy_scout_2_img.h
@@ -4,8 +4,8 @@
 // SVG Graphic source: svg_docs/enemies_output_from_ai/scout_2_img.svg
 // Number bytes 35
 const PROGMEM BYTE enemy_scout_2_img[] = {
-    128,    // Width (128 px)
-    46,    // Height (46 px)
+    // 128,    // Width (128 px)
+    // 46,    // Height (46 px)
     8,    // Number of rows of coords (8)
 //  x0,     y0,    x1,    y1
     -9,    -23,    37,    23,
