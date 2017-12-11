f0e192077e89d2a407cb4cef173cf797c3413fd6
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-11 09:39:41 -0500

    Using anohter image for scout1

--- evade2/img/enemy_scout_1_img.h
+++ evade2/img/enemy_scout_1_img.h
@@ -1,21 +1,22 @@
 
 #ifndef ENEMY_SCOUT_1_IMG_H
 #define ENEMY_SCOUT_1_IMG_H
-// SVG Graphic source: svg_docs/enemies_output_from_ai/scout_1_img.svg
+// SVG Graphic source: svg_docs/output_from_ai/assault_1_img.svg
 // Number bytes 35
 const PROGMEM BYTE enemy_scout_1_img[] = {
-  128, // Width (128 px)
-  97,  // Height (97 px)
-  8,   // Number of rows of coords (8)
-       //  x0,     y0,    x1,    y1
-  -64, -48, -32, -16,
-  -32, -16, -32, 32,
-  -16, 0, -16, 0,
-  64, -48, 32, -16,
-  32, -16, 32, 32,
-  16, 0, 16, 0,
-  -48, -16, 0, 32,
-  0, 32, 48, -16,
+    128,    // Width (128 px)
+    38,    // Height (38 px)
+    8,    // Number of rows of coords (8)
+//  x0,     y0,    x1,    y1
+    9,    0,    27,    19,
+    -27,    19,    -9,    0,
+    -18,    -9,    -27,    0,
+    -27,    0,    -9,    19,
+    9,    19,    27,    0,
+    27,    0,    18,    -9,
+    -27,    19,    -64,    -19,
+    27,    19,    64,    -19,
 
 };
 #endif
+
