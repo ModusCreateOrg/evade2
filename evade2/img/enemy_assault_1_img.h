ac552bfcf536a68d2289e6f6b5b305339fbff3ee
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-10 20:39:09 -0500

    Adding new assault-1 image to the repo

--- evade2/img/enemy_assault_1_img.h
+++ evade2/img/enemy_assault_1_img.h
@@ -1,21 +1,28 @@
 
 #ifndef ENEMY_ASSAULT_1_IMG_H
 #define ENEMY_ASSAULT_1_IMG_H
-// SVG Graphic source: svg_docs/enemies_output_from_ai/assault_1_img.svg
-// Number bytes 35
+// SVG Graphic source: svg_docs/output_from_ai/assault-1.svg
+// Number bytes 59
 const PROGMEM BYTE enemy_assault_1_img[] = {
     128,    // Width (128 px)
-    38,    // Height (38 px)
-    8,    // Number of rows of coords (8)
+    54,    // Height (54 px)
+    14,    // Number of rows of coords (14)
 //  x0,     y0,    x1,    y1
-    9,    0,    27,    19,
-    -27,    19,    -9,    0,
-    -18,    -9,    -27,    0,
-    -27,    0,    -9,    19,
-    9,    19,    27,    0,
-    27,    0,    18,    -9,
-    -27,    19,    -64,    -19,
-    27,    19,    64,    -19,
+    -21,    -27,    21,    16,
+    -21,    16,    21,    -27,
+    21,    -16,    11,    -5,
+    11,    -5,    21,    5,
+    -21,    5,    -11,    -5,
+    -11,    -5,    -21,    -16,
+    -11,    27,    -32,    5,
+    -32,    5,    -21,    -5,
+    11,    27,    32,    5,
+    32,    5,    21,    -5,
+    -21,    -16,    -64,    -16,
+    21,    -16,    64,    -16,
+    -11,    5,    0,    16,
+    0,    16,    11,    5,
 
 };
 #endif
+
