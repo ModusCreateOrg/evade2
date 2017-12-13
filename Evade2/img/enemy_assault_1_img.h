01a0309f0e9448e90953c873b0f57a53e373b4b7
Delio Brignoli <brignoli.delio@gmail.com>
2017-12-13 06:33:38 +0000

    Fix compiler warnings/errors related to unsigned/signed assignments

--- Evade2/img/enemy_assault_1_img.h
+++ Evade2/img/enemy_assault_1_img.h
@@ -3,10 +3,16 @@
 #define ENEMY_ASSAULT_1_IMG_H
 // SVG Graphic source: svg_docs/output_from_ai/assault-1.svg
 // Number bytes 59
-const PROGMEM BYTE enemy_assault_1_img[] = {
-    128,    // Width (128 px)
-    54,    // Height (54 px)
-    14,    // Number of rows of coords (14)
+const PROGMEM struct enemy_assault_1_img {
+	uint8_t w;
+	uint8_t h;
+	uint8_t r;
+	int8_t data[14*4];
+} enemy_assault_1_img = {
+	.w = 128,    // Width (128 px)
+	.h = 54,    // Height (54 px)
+	.r = 14,    // Number of rows of coords (14)
+	.data = {
 //  x0,     y0,    x1,    y1
     -21,    -27,    21,    16,
     -21,    16,    21,    -27,
@@ -22,7 +28,7 @@ const PROGMEM BYTE enemy_assault_1_img[] = {
     21,    -16,    64,    -16,
     -11,    5,    0,    16,
     0,    16,    11,    5,
-
+},
 };
 #endif
 
