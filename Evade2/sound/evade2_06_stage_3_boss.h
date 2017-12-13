8ead0552a3b8e3b96f9e1649cac8898afcf6a4ab
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-13 14:33:23 -0500

    Another 2 bytes off of boss 3 track

--- Evade2/sound/evade2_06_stage_3_boss.h
+++ Evade2/sound/evade2_06_stage_3_boss.h
@@ -26,17 +26,16 @@
 }
 DEFINE_PATTERN(evade2_06_stage_3_boss_pattern0_array, evade2_06_stage_3_boss_pattern0_data);
     
-/* pattern (channel) / bytes = 3*/
+/* pattern (channel) / bytes = 1*/
 #define evade2_06_stage_3_boss_pattern1_data { \
-    ATM_CMD_M_SET_VOLUME(110), \
-    ATM_CMD_M_SET_LOOP_PATTERN(1), \
     ATM_CMD_I_STOP, \
 }
 DEFINE_PATTERN(evade2_06_stage_3_boss_pattern1_array, evade2_06_stage_3_boss_pattern1_data);
     
-/* pattern (channel) / bytes = 13*/
+/* pattern (channel) / bytes = 15*/
 #define evade2_06_stage_3_boss_pattern2_data { \
     ATM_CMD_M_SET_VOLUME(110), \
+    ATM_CMD_M_NOTECUT_ON(0), \
     ATM_CMD_M_CALL_REPEAT(9, 3), \
     ATM_CMD_M_CALL(10), \
     ATM_CMD_M_CALL_REPEAT(9, 3), \
