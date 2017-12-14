f9876632b5bb745a3d54d868db2e7437c945d3fd
Michael Schwartz <mykesx@gmail.com>
2017-12-14 15:39:27 -0800

    reduce code size

--- Evade2/Logo.cpp
+++ Evade2/Logo.cpp
@@ -6,19 +6,26 @@
   * Show Modus Create logo
   */
 
+struct logo_data {
+  BYTE x, y;
+  WORD timer;
+  BOOL scrolled;
+};
+
 void Logo::loop(Process *me, Object *o) {
-  if (o->y < 8) {
-    o->y++;
+  logo_data *d = (logo_data *)&o->x;
+  if (d->y < 8) {
+    d->y++;
   }
-  else if (!o->state) {
-    o->timer = 90;
-    o->state = TRUE;
+  else if (!d->scrolled) {
+    d->timer = 90;
+    d->scrolled = TRUE;
   }
-  Graphics::drawBitmap(o->x, o->y, &modus_logo_img[2], 0x2a, 0x28);
-  if (o->state) {
+  Graphics::drawBitmap(d->x, d->y, &modus_logo_img[2], 0x2a, 0x28);
+  if (d->scrolled) {
     Font::printf(8, 56, "Modus Create");
   }
-  if (--o->timer < 0) {
+  if (--d->timer < 0) {
     ProcessManager::birth(Splash::entry);
     me->suicide();
   }
@@ -28,9 +35,12 @@ void Logo::loop(Process *me, Object *o) {
 }
 
 void Logo::entry(Process *me, Object *o) {
+  logo_data *d = (logo_data *)&o->x;
+  
   game_mode = MODE_LOGO;
-  o->x = 40;
-  o->y = -40;
-  o->timer = 48;
+  d->x = 40;
+  d->y = -40;
+  d->timer = 48;
+  d->scrolled = false;
   me->sleep(1, Logo::loop);
 }
