74178e1c8ac2ddfcf184199eafc948dc893475bb
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-14 16:48:15 -0500

    Cleaned up use of rkeys in Controlls.cpp

--- Evade2/Controls.cpp
+++ Evade2/Controls.cpp
@@ -2,19 +2,20 @@
 
 #include "Evade2.h"
 
-UBYTE Controls::rkeys = 0;
+// UBYTE Controls::rkeys = 0;
 UBYTE Controls::ckeys = 0;
 UBYTE Controls::dkeys = 0;
 
 void Controls::reset() {
-  rkeys = ckeys = dkeys = 0;
+  ckeys = dkeys = 0;
+  // rkeys = ckeys = dkeys = 0;
 }
 
 void Controls::run() {
   UBYTE buttons = arduboy.buttonsState();
   dkeys = (buttons ^ ckeys) & buttons;
   ckeys = buttons;
-  rkeys = buttons;
+  // rkeys = buttons;
 }
 
 BOOL Controls::debounced(UBYTE key) {
