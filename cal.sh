648d413c608e23dfae35a3f3cff8e14b094f26b6
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-15 17:20:08 -0500

    introduction of cal.sh (compile and load) shell script for mac and linux

---
+++ cal.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+which git 
+if [ $? -gt 0 ]; then 
+    echo "ERROR:\nCould not find git in your \$PATH"
+    exit 1
+fi
+
+echo "Grabbing git submodules"
+git submodule init; git submodule update;
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+arduino_dir=""
+if [ "${machine}" == "Mac" ]; then
+    arduino_dir="`pwd`/tools/arduino-ide/mac"
+fi
+
+#TODO: Linux
+
+if [ -z $arduino_dir ]; then
+    echo "ERROR! Could not find arduino dir for your platform (${unameOut})"
+    exit 1
+fi
+
+
+
+
+
+cd Evade2/ 
+make ARDUINO_DIR="${arduino_dir}"
