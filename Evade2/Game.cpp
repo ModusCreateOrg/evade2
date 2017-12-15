c2457833024962be9390cd5695b374a41e438a1a
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-15 13:07:44 -0500

    Changed the flow of kills required per wave. Slower rampup

--- Evade2/Game.cpp
+++ Evade2/Game.cpp
@@ -69,8 +69,12 @@ void Game::spawn_boss(Process *me, Object *o) {
 
 void Game::run() {
   // if (Game::kills > 0) { // <<-- use this one to debug quickly
-  // Gets hard really quickly!
-  if (Game::kills > (4 * Game::wave) * Game::difficulty) {
+  // Faster increase 
+  // if (Game::kills > (4 * Game::wave) * Game::difficulty) {
+  
+  // Slower increase 
+  if (Game::kills > (5 * Game::difficulty) + Game::wave) {
+
     game_mode = MODE_NEXT_WAVE;
     // next wave
     Game::kills = 120;
