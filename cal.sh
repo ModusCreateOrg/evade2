e0749e9a15396bacc0fc57dff3c6fe1ede56f1f5
Jay Garcia <jesusdgarcia@gmail.com>
2017-12-16 11:33:51 -0500

    Added powercycle warning to cal.sh

--- cal.sh
+++ cal.sh
@@ -87,5 +87,6 @@ fi
 
 ${arduino_dir}/hardware/tools/avr/bin/avr-size --mcu=atmega32u4 -C --format=avr Evade2/build-leonardo/Evade2.elf
 
+echo "*IMPORTANT!*\nYou must power cycle your Arduino!"
 
 
