d352f088a781786b27e15cb405ce8a59e1812a81
Delio Brignoli <brignoli.delio@gmail.com>
2018-01-14 14:09:35 +0100

    Disable logo for testing

--- Evade2/Evade2.h
+++ Evade2/Evade2.h
@@ -31,7 +31,7 @@
 
 // Modus Create LOGO splash screen first time
 #define ENABLE_MODUS_LOGO
-// #undef ENABLE_MODUS_LOGO
+#undef ENABLE_MODUS_LOGO
 
 // If FAST_LINE_ENABLE is set, the fast line drawing algorithm is used
 // While it is a bit faster, it seems to have some rounding errors that
